021ba05543711e4bb1f310204412a1a12d73f005
Robert Gitei <robert.gitei@yopeso.com>
2022-08-31 16:21:16 +0300

    3. State in Compose

---
+++ app/src/main/java/com/example/composestatecodelab/WaterCounter.kt
@@ -0,0 +1,16 @@
+package com.example.composestatecodelab
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    val count = 0
+    Text(
+        text = "ai baut $count pahare de apa",
+        modifier = modifier.padding(16.dp)
+    )
+}
